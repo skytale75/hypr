81892935eff236668a1cfd2f828969bc9205f5d1
skytale75 <guinnessmanmike@gmail.com>
2025-08-16 21:40:44 -0700

    remove junk

--- scripts/active_submap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-makoctl mode -a hyprsubmaps
-notify-send "$(hyprctl activeworkspace -j | jq -r .name)"
-makoctl mode -r hyprsubmaps
-
--- scripts/program_exit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Move the window to the center of the screen
-hyprctl dispatch killactive 
-sleep 0.03
-hyprctl dispatch scroller:alignwindow c &
-hyprctl dispatch scroller:alignwindow m
