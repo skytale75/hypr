2cbbf96ef3d8ad2722ca9a555089d13811b43348
skytale75 <guinnessmanmike@gmail.com>
2025-10-30 09:41:50 -0700

    october 30

--- scripts/resize_center.sh
+++ scripts/resize_center.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Resize the active window to 85% of the monitor dimensions
-hyprctl dispatch resizeactive exact $1 95%
+hyprctl dispatch resizeactive exact $1 $2
 
 # Center the newly resized window on the active monitor
 hyprctl dispatch centerwindow
